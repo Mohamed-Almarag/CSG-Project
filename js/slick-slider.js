50363853ab07e1764c99656f2d2fe281af2906ad
Mohamed-Almarag <mohamedalmarag97@gmail.com>
2023-10-23 20:25:15 +0300

    maps footer navbar

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -1,19 +1,68 @@
+// Slider For Hero Section
 $(() => {
-  $(".responsive").slick({
-    dots: false,
+  $(".csg_hero_slider .responsive").slick({
+    dots: true,
     infinite: false,
-    arrows: true,
+    arrows: false,
     speed: 300,
-    slidesToShow: 4,
+    slidesToShow: 1,
     slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
           dots: true,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
+
+
+$(() => {
+  $(".csg_customers_slider .responsive").slick({
+    dots: false,
+    infinite: true,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+          arrows: true,
         },
       },
       {
@@ -41,3 +90,5 @@ $(() => {
     ],
   });
 });
+
+
